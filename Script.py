b91810b7ca1e3e3a62f8dbc0e21d3fa637919eca
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-08-10 23:36:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -218,14 +218,14 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     MOVIE_UPDATE_NOTIFY_TXT = """
 </b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
 
-<blockquote>✨ ᴛɪᴛʟᴇ : <code>{filename}</code>
+</b>✨ ᴛɪᴛʟᴇ : <code>{filename}</code>
 
 🎭 ɢᴇɴʀᴇs : <b>{genres}</b>
 🎞️ ǫᴜᴀʟɪᴛʏ : <b>{quality}</b>
 🎧 ᴀᴜᴅɪᴏ    : <b>{language}</b>
 🔥 ʀᴀᴛɪɴɢ   : <b>{rating}</b>
 {episodes}
-</blockquote>
+</b>
 
 🔍 <b>Sᴇᴀʀᴄʜ →</b> {search_link}
 """
