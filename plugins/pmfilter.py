927734f8a75b8999203e249c230edcf54d493860
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-01 22:15:50 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2030,11 +2030,11 @@ async def auto_filter(client, msg, spoll=False):
                 cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
     else:
         if settings.get('button'):
-            cap = f"<b>🧾 <u>Your Requested Files Are Here</u> 👇 \n\n</b>"
+            cap = f"<b>🧾 <u>Your Requested Files Are Here</u> 👇 \n</b>"
         else:
-            cap = f"<b>🧾 <u>Your Requested Files Are Here</u> 👇 \n\n</b>"
+            cap = f"<b>🧾 <u>Your Requested Files Are Here</u> 👇 \n</b>"
 
-            for idx, file in enumerate(files, start=1):
+            for idx, file in enumerate(files, start=1🌿):
                 cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
     if imdb and imdb.get('poster'):
         try:
