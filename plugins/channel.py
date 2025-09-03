2abb71fa3baf343c78913ffa3cc03eff7e4ee4b9
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-03 23:46:22 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -397,6 +397,12 @@ async def update_movie_message(bot, base_name):
             return
 
         text, buttons = generate_movie_message(movie_doc, base_name)
+        buttons = InlineKeyboardMarkup([[
+            InlineKeyboardButton(
+                'ɢᴇᴛ ғɪʟᴇs',
+                url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
+            )
+        ]])
 
         message_id = movie_doc.get("message_id")
         is_photo = movie_doc.get("is_photo", False)
