d1506973484753a5bcf79d2cd002ba7b44523e7a
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-07 15:47:06 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1543,6 +1543,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]]
         except Exception as e:    
             pass
+         await client.edit_message_media(
+                query.message.chat.id, 
+                query.message.id, 
+                InputMediaPhoto(random.choice(PICS))
+            )
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
