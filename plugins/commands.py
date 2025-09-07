007ff0d16b299b82c67c489ed0c584501db83f65
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-08 00:29:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,8 +115,8 @@ async def start(client, message):
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
+        
         return
-
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
