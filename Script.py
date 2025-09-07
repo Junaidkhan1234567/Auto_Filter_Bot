dd69b67994e217a8e693346aea91bf6e592cabec
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-07 17:16:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,8 @@
 class script(object):
-    START_TXT = """<b>🤖 ɪ ᴀᴍ <a href=https://t.me/{}>{}</a>\n🍿 Unlimited Movies\n🎬 Instant File Access\n🆓 100% Free, Always\n🔮 By @WOLVERIN_P 🌿</b>"""
+    START_TXT = """<b><u>🤖 ɪ ᴀᴍ <a href=https://t.me/{}>{}</a>\n🍿 Unlimited Movies\n🎬 Instant File Access\n🆓 100% Free, Always\n🔮 By @WOLVERIN_P 🌿</u></b>"""
 
 
-    GSTART_TXT = """<b>🤖 ɪ ᴀᴍ <a href=https://t.me/{}>{}</a>\n🍿 Unlimited Movies\n🎬 Instant File Access\n🆓 100% Free, Always\n🔮 By @WOLVERIN_P 🌿</b>"""
+    GSTART_TXT = """<b><u>🤖 ɪ ᴀᴍ <a href=https://t.me/{}>{}</a>\n🍿 Unlimited Movies\n🎬 Instant File Access\n🆓 100% Free, Always\n🔮 By @WOLVERIN_P 🌿</u></b>"""
 
     
     HELP_TXT = """<b>
