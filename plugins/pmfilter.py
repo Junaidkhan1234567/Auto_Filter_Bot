1965c012083be1af22e5aefd865b6aec77d11822
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2025-09-08 00:20:08 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1549,7 +1549,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.START_TXT.format(query.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        )
+        
         await query.answer(MSG_ALRT)
 
     elif query.data == "donation":
