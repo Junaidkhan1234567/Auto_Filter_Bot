b8ade0db01516993b4a3c850671b0f257903918d
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2026-06-03 12:37:15 +0530

    Update PREMIUM_LOGS channel ID

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002016227618'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002016227618'))  # Bin channel id (make sure bot is admin)
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1001521000125'))  # Premium logs channel id
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002229751777'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-100').split()] #(make sure bot is admin)
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001521000125')  # Support group id (make sure bot is admin)
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001521000125')  # Request channel id (make sure bot is admin)
