ee7567b8c502dc1c7a2ba653c3990d59026bb1ba
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2026-06-07 15:27:12 +0530

    Update commands_modified_quality_toggle.py

--- plugins/commands_modified_quality_toggle.py
+++ plugins/commands_modified_quality_toggle.py
@@ -399,7 +399,7 @@ async def start(client, message):
             if STREAM_MODE and not PREMIUM_STREAM_MODE:
                 btn = [
                     [InlineKeyboardButton('🚀 ꜰᴀꜱᴛ ᴅᴏᴡɴʟᴏᴀᴅ / ᴡᴀᴛᴄʜ ᴏɴʟɪɴᴇ 🖥️', callback_data=f'generate_stream_link:{file_id}')],
-                    [InlineKeyboardButton(''🔞 𝙼𝙰𝚂𝚃𝙸 𝚃𝙸𝙼𝙴 📌', url=UPDATE_CHNL_LNK)]  # Keep this line unchanged  
+                    [InlineKeyboardButton('🔞 𝙼𝙰𝚂𝚃𝙸 𝚃𝙸𝙼𝙴 📌', url=UPDATE_CHNL_LNK)]  # Keep this line unchanged  
                 ]
             elif STREAM_MODE and PREMIUM_STREAM_MODE:
                 if not await db.has_premium_access(message.from_user.id):
